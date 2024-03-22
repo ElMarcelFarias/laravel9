abd7ca77b162a195871705e53abd155cb9fbe1fc
ElMarcelFarias <marcel.leitefarias@gmail.com>
2024-03-22 14:10:02 -0300

    Feat: Finalizando Aula 12 - Laravel9 trabalhando com templates Blade

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,15 +2,34 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
     public function index() {
-        return view('users.index');
+
+        $users = User::get(); //lista de todos os usuários
+        
+
+        // return view('users.index', [
+        //     'users' => $users
+        // ]); //podemos passar assim ou 
+
+        return view('users.index', compact('users'));
     }
 
     public function show($id) {
-        dd('users.show', $id);
+
+        //recuperar o usuário pelo id
+        //Primeira forma
+        //$user = User::where('id', $id)->first(); //->get() me retorna uma collection, ou seja um array. 
+                                                //Eu necessito que me retorne um objeto daquele usuário, nao um array, entao utilizamos o first()
+
+        if(!$user = User::find($id)) {
+            return redirect()->route('users.index');
+        }
+        
+        return view('users.show', compact('user'));
     }
 }
---
+++ resources/views/users/show.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+
+@section('title', 'Listagem do Usuário')
+
+@section('content')
+
+<h1>Listagem de usuários - Detalhes {{ $user->name }}</h1>
+
+<ul>
+    <li>{{ $user->name }}</li>
+    <li>{{ $user->email }}</li>
+    <li>{{ $user->created_at }}</li>
+</ul>
+
+@endsection
+
