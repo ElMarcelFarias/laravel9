8e9e194da1fb5ec294d7f4a7bd883e82ae984202
ElMarcelFarias <marcel.leitefarias@gmail.com>
2024-03-26 14:21:04 -0300

    Feat: Aula 20 - Incluindo CDN do tailwind

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,11 +4,15 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="shortcut icon" href="{{ url('images/favicon.ico') }}" type="image/png">
+
+    <script src="https://cdn.tailwindcss.com"></script>
     <title>@yield('title')</title>
 </head>
-<body>
+<body class="bg-gray-50">
     
-    <div class="app">
+    <div class="container mx-auto px-4 py-8">
         @yield('content')
     </div>
 
--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -3,11 +3,11 @@
 @section('title', 'Listagem dos Usuários')
 
 @section('content')
-<h1>Listagem de usuários <a href="{{ route('users.create') }}"> (+)</a></h1>
+<h1 class="text-2x1 font-semibold leading-tigh py-2">Listagem de usuários <a href="{{ route('users.create') }}" class="bg-blue-900 rounded-full text-white px-4 text-sm"> (+)</a></h1>
 
-<form action="{{ route('users.index') }}" method="GET">
-    <input type="text" name="search" placeholder="pesquisar usuário...">
-    <button>Pesquisar</button>
+<form action="{{ route('users.index') }}" method="GET" class="py-5">
+    <input type="text" name="search" placeholder="pesquisar usuário..." class="md:w-1/6 bg-gray-200 appearance-none">
+    <button class="shadow bg-purple-500 hover:bg-purple">Pesquisar</button>
 </form>
 
 <ul>
